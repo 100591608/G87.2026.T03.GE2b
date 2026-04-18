45bda16a6fddd92d64a17b3c33101d8bea626eaa
pablo-velarde <100591608@alumnos.uc3m.es>
2026-04-18 11:34:35 +0200

    Begin writing code for register_document test cases, setting up imports and global variables.

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -1,6 +1,20 @@
-"""class for testing the regsiter_order method"""
+"""Class for testing the register_document method"""
 import unittest
-from uc3m_consulting import EnterpriseManager
+import csv
+import json
+import os.path
+import hashlib
+from unittest import TestCase
+from os import remove, makedirs
+from freezegun import freeze_time
+from uc3m_consulting import (
+    EnterpriseManager,
+    EnterpriseManagementException
+)
+
+JSON_FILES_PATH = "json_files/"
+GENERATED_INPUTS_PATH = JSON_FILES_PATH + "generated_inputs/"
+ALL_DOCUMENTS_STORE_FILE = JSON_FILES_PATH + "all_documents.json"
 
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
