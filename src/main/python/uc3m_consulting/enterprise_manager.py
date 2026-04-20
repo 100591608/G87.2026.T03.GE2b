e19c2fb91c78d1730e4c056f1fe07310e95b3f18
100578087 <100578087@alumnos.uc3m.es>
2026-04-20 23:45:24 +0200

    Add minimum source code to pass 53 more tests.

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,6 +1,7 @@
 """Module """
 import json
 from uc3m_consulting.project_document import ProjectDocument
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
 
 ALL_DOCUMENTS_PATH = "../../unittest/python/json_files/all_documents.json"
 
@@ -11,14 +12,17 @@ class EnterpriseManager:
 
     @staticmethod
     def validate_cif(cif: str):
-        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
+        """RETURNS TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
         OR FALSE IN OTHER CASE"""
         return True
 
     def register_document(self, input_file: str):
         """Registers document in the Enterprise Manager"""
-        with open(input_file, "r", encoding="utf-8", newline="") as file:
-            input_data = json.load(file)
+        try:
+            with open(input_file, "r", encoding="utf-8", newline="") as file:
+                input_data = json.load(file)
+        except json.JSONDecodeError as ex:
+            raise EnterpriseManagementException("The file is not JSON formatted") from ex
 
         document = ProjectDocument(input_data["PROJECT_ID"], input_data["FILENAME"])
 
